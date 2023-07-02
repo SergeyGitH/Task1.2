b6f2b67b8e7e7c60e6862b3965b61c61a3f3cacd
”SergeyGitH” <reply.s@yandex.ru>
2023-07-02 12:51:00 +0300

    Доработка задания 2.2

--- init.sql
+++ init.sql
@@ -5,20 +5,11 @@ CREATE TABLE IF NOT EXISTS public.readers (
     surname VARCHAR(255) NOT NULL,
     patronymic VARCHAR(255) NOT NULL
 );
-
-CREATE TABLE IF NOT EXISTS public.rent (
-    rent_id serial PRIMARY KEY,
-    data DATE,
-    FOREIGN KEY (reader_id) REFERENCES readers (reader_id) ON DELETE SET NULL,
-    FOREIGN KEY (book_id) REFERENCES books (book_id) ON DELETE SET NULL
-);
-
 CREATE TABLE IF NOT EXISTS public.publisher (
     publisher_id serial PRIMARY KEY,
     name_publisher VARCHAR(255) NOT NULL,
     city VARCHAR(255) NOT NULL
 );
-
 CREATE TABLE IF NOT EXISTS public.books (
     book_id serial PRIMARY KEY,
     code BIGINT NOT NULL,
@@ -27,10 +18,21 @@ CREATE TABLE IF NOT EXISTS public.books (
     quantity VARCHAR(255) NOT NULL,
     FOREIGN KEY (publisher_id) REFERENCES publisher (publisher_id) ON DELETE SET NULL
 );
+
+CREATE TABLE IF NOT EXISTS public.rent (
+    rent_id serial PRIMARY KEY,
+    data DATE,
+    FOREIGN KEY (reader_id) REFERENCES readers (reader_id) ON DELETE SET NULL,
+    FOREIGN KEY (book_id) REFERENCES books (book_id) ON DELETE SET NULL
+);
 CREATE TABLE IF NOT EXISTS public.author (
     author_id serial PRIMARY KEY,
     last_name VARCHAR(255) NOT NULL,
     surname VARCHAR(255) NOT NULL,
     patronymic VARCHAR(255) NOT NULL,
     birth_year VARCHAR(255) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS public.books-author (
+    FOREIGN KEY (book_id) REFERENCES books (books_id) ON DELETE SET NULL,
+    FOREIGN KEY (author_id) REFERENCES author (author_id) ON DELETE SET NULL
 );
